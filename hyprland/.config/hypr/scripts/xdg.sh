1d63516d8abbb61fd848242f4766667e7ca3b8d7
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-06-09 01:48:48 +0700

    fix: update autostart processes and improve xdg script for better environment handling

--- hyprland/.config/hypr/scripts/xdg.sh
+++ hyprland/.config/hypr/scripts/xdg.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-sleep 4
-killall -e xdg-desktop-portal-hyprland
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-lxqt
+killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
+sleep 1
+
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 4
+sleep 2
 /usr/lib/xdg-desktop-portal &
-sleep 4
+sleep 2
 
 # Fix for dolphin not showing apps on open with 
 # first, install: sudo pacman -S archlinux-xdg-menu
