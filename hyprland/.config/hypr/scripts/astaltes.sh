9936bef9a49fcfc2f3bf0d0560b10399e9389a0e
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-06-16 09:48:00 +0700

    autogen

--- hyprland/.config/hypr/scripts/astaltes.sh
+++ hyprland/.config/hypr/scripts/astaltes.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m ~/.local/share/astaltes/astaltes
+LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m ~/.local/share/astaltes/astaltes > ~/.local/share/astaltes/astaltes.log 2>&1
