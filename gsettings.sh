a65b110d94599da71920182e439797d07a7bf08e
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-07-15 00:40:54 +0700

    feat: add gsettings script for GNOME desktop customization

---
+++ gsettings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+gsettings set org.gnome.desktop.interface color-scheme \'prefer-dark\'
+gsettings set org.gnome.desktop.interface font-antialiasing \'rgba\'
+gsettings set org.gnome.desktop.interface font-hinting \'slight\'
+gsettings set org.gnome.desktop.interface font-name "'Noto Sans 11'"
+gsettings set org.gnome.desktop.interface font-rendering \'automatic\'
+gsettings set org.gnome.desktop.interface font-rgba-order \'rgb\'
+gsettings set org.gnome.desktop.interface cursor-theme \'Bibata-Modern-Amber\'
+gsettings set org.gnome.desktop.interface cursor-size 20
